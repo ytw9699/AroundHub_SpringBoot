7d4ec0fa2d2f4274028e8cb589f10695718046a9
home <ytw9699>
2023-03-15 07:31:18 +0900

    feat : 레디스 테스트 레디스 템플릿

---
+++ src/test/java/studio/thinkground/aroundhub/test/redisTest.java
@@ -0,0 +1,96 @@
+package studio.thinkground.aroundhub.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.Set;
+
+@SpringBootTest()
+public class redisTest {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @DisplayName("레디스 String 값 insert, read 테스트")
+    @Test
+    public void insertAndReadStringInRedis() {
+
+        String key = "testStringKey";
+        String value = "testStringValue";
+
+        ValueOperations<String, String> valueOperations =
+                    redisTemplate.opsForValue();
+
+        valueOperations.set(key, value);
+
+        String resultValue = (String)redisTemplate.opsForValue().get(key);
+
+        assertThat(value).isEqualTo(resultValue);
+    }
+
+    @DisplayName("레디스 String 값 insert, read 테스트")
+    @Test
+    public void insertAndReadStringInRedis2() {
+
+        String key = "testStringKey";
+        String value = "testStringValue";
+
+        redisTemplate.opsForValue().set(key, value);
+
+        String resultValue = (String)redisTemplate.opsForValue().get(key);
+
+        assertThat(value).isEqualTo(resultValue);
+    }
+
+    @DisplayName("레디스 delete 테스트")
+    @Test
+    public void deleteStringInRedis() {
+
+        String key = "testStringKey";
+
+        assertTrue(redisTemplate.delete(key));
+    }
+
+    @DisplayName("레디스 set 테스트")
+    @Test
+    void testSet() {
+        // given
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        String key = "testSetKey";
+
+        // when
+        setOperations.add(key, "h", "e", "l", "l", "o");
+
+        // then
+        Set<String> members = setOperations.members(key);
+        Long size = setOperations.size(key);
+
+        assertThat(members).containsOnly("h", "e", "l", "o");
+        assertThat(size).isEqualTo(4);
+    }
+
+    @DisplayName("레디스 hash 테스트")
+    @Test
+    void testHash() {
+
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+
+        String key = "testHashKey";
+        String hashKey = "hello";
+        String value = "world";
+
+        hashOperations.put(key, hashKey, value);
+
+        Object result = hashOperations.get(key, hashKey);
+
+        assertThat(value).isEqualTo(result);
+    }
+}
